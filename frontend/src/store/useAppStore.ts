af78d3db3bb1774aef205c4651002ea1290c49b8
GusVelez9 <gf.velez9@gmail.com>
2026-06-14 22:33:51 -0500

    Update useAppStore.ts

--- frontend/src/store/useAppStore.ts
+++ frontend/src/store/useAppStore.ts
@@ -24,15 +24,15 @@ export const useAppStore = create<AppStore>((set) => ({
     // Esta función hace la magia de conectarse al backend
     fetchDatabase: async () => {
         try {
-            const response = await fetch('https://nutri-j3ph.onrender.com/api/sync');
+            const response = await fetch('http://localhost:3000/api/sync');
             const result = await response.json();
 
             if (result.status === 'success') {
-                return result.data;
+                set({ dbData: result.data, isLoaded: true });
+                console.log("✅ ¡Catálogo descargado y listo para modo offline!", result.data);
             }
-
         } catch (error) {
-            console.error("Error fetching database:", error);
+            console.error("❌ Error conectando al backend:", error);
         }
-    }    
+    }
 }));
